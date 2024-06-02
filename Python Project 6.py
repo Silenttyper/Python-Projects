8c1d3fcf5d51b91ea39fdd4071dc34e5c481331b
Silenttyper <nuwile.mulbah@my.century.edu>
2024-06-02 13:51:56 -0500

    Python Project 6.py

--- Python Project 6.py
+++ Python Project 6.py
@@ -1,32 +1,45 @@
+#Timed Math Quiz Game
 import random
+import keyboard
 import time
 
-OPERATORS=["+","-","*"]
-MIN_OPERATOR=3
-MAX_OPERATOR=12
-TOTAL_PROBLEMS=10
-def generate_problem():
-    left=random.randint(MIN_OPERATOR,MAX_OPERATOR)
-    right=random.randint(MIN_OPERATOR,MAX_OPERATOR)
-    operator=random.choice(OPERATORS)
-    expr = str(left) + " "+ operator + " " + str(right)
-    answer=eval(expr)
-    return expr,answer
+MINNUMBER=3
+MAXNUMBER=12
+OPERATIONS=["+","-","*"]
+TOTALQUESTIONS=10
 
-wrong=0 #This means they have zero questions wrong
-print(input("Press (enter) to start"))
-print("-----------------------")
-start_time=time.time() #Counts in seconds since time start
+def mathproblem():
+    left=random.randint(MINNUMBER,MAXNUMBER)
+    right=random.randint(MINNUMBER,MAXNUMBER)
+    operator=random.choice(OPERATIONS)
+    mathequation=str(left) + " " + operator + " " + str(right)
+    answer=eval(mathequation)
+    return mathequation,answer
 
 
-for xyz in range(TOTAL_PROBLEMS):
-    expr,answer= generate_problem()
+def spacebar():
+    print("Press space to start")
     while True:
-        guess=input("Problem #" + str(xyz+1) + ":" + expr + "= ")
-        if guess==str(answer):
+        if keyboard.is_pressed("space"):
             break
-        wrong==1 #This means if they get the question wrong python will add 1 to wrong answers every time
-end_time=time.time() #You use time.time() to end the time count
-total_time= round(end_time-start_time)
-print("-----------------------")
-print(f"Nice work. You finished in {total_time} seconds")
+        time.sleep(0.1)
+spacebar()
+
+
+wrong =0
+start_time=time.time()
+
+for y in range(TOTALQUESTIONS):
+    mathequation,answer=mathproblem()
+    user_answer=input("Questions " + str(y+1) + ": " + " " + mathequation + " = ")
+    if user_answer==str(answer):
+        continue
+    else:
+        wrong+=1
+   
+
+end_time=time.time()
+total_time=round(end_time-start_time)
+print(f"You finished the math quiz in {total_time} seconds.")
+
+
