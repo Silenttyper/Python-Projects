f0b84536dbaacac9f8b5c95f7ff73d67a3d50d25
Silenttyper <nuwile.mulbah@my.century.edu>
2024-06-02 13:00:21 -0500

    Add files via upload

---
+++ Python Project 5.py
@@ -0,0 +1,32 @@
+import random
+
+def create_password():
+    user_password=input("Create your password ")
+    confirm_password=input("Confirm your password ")
+    if user_password==confirm_password:
+        return user_password
+    else:
+        return "Passwords dont match. Try again"
+
+
+capital_letters="WERTYUIOPASDFGHJKLZXCVBNM"
+lowercase_letters="qwertyuiopasdfghjklzxcvbnm"
+integer="1234567890"
+special_characters="!@#$%^&*()_+=|\{}[]:;,./<>?"
+upper,lower,digits,odd=True,True,True,True
+
+charactersinpassword=""
+if upper:
+    charactersinpassword+=capital_letters
+if lower:
+    charactersinpassword+=lowercase_letters
+if digits:
+    charactersinpassword+=integer
+if odd:
+    charactersinpassword+=special_characters
+
+howmanypasswords=1
+
+for z in range(howmanypasswords):
+    new_password=create_password()
+    print(f"Your new password is {new_password}")
